95ca0223695fc4e857e267a790674703ca893ff1
Jason Everton <jasonemsw10@gmail.com>
2022-11-19 14:19:06 -0300

    Refatorando o Users_add foreign_key_id

--- src/shared/infra/typeorm/migrations/1668862130961-add_update_users_id.ts
+++ src/shared/infra/typeorm/migrations/1668862130961-add_update_users_id.ts
@@ -10,6 +10,7 @@ export class CreateUsers1668271245616 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true
           },
           {
             name: "name",
